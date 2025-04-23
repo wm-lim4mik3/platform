e1edbcc3b07e937878a26ea44f9a1e7d288770a2
Leonardo Mendes <leonardo.mendes@webmotors.com.br>
2025-04-23 10:10:46 -0300

    feat: adding this new feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
 	return (
 		<div>
-			<h1>First commit</h1>
-			<h1>Second Commit</h1>
-			<h1>Third Commit</h1>
+			<span>cabelin no hair</span>
 		</div>
 	);
 }
